eeb8669f5a9c7655cd3d7f2c87eb300df28b7f27
Rahul Roy <devrahul2508@gmail.com>
2022-09-26 13:44:40 +0530

    Commits

--- routes/user.js
+++ routes/user.js
@@ -1,12 +1,47 @@
 const router = require("express").Router();
+const { verifyToken, verifyTokenAndAuthentication, verifyTokenAndAdmin } = require("./verifyToken")
+const User = require("../models/User")
 
-router.get("/usertest",(req,res)=>{
-    res.send("user test is successfull");
-});
+//UPDATE USER
+router.put("/:id",verifyTokenAndAuthentication,async (req,res)=>{
+    if(req.body.password){
+        req.body.password = CryptoJS.AES.encrypt(req.body.password, process.env.PASS_SEC)
+    }
 
-router.post("/userposttest",(req,res)=>{
-   const username = req.body.username;
-   console.log(username);
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id,{
+            $set: req.body
+        },{new:true})
+        res.status(200).json(updatedUser);
+    }catch(err){
+        res.status(500).json(err);
+    }
 })
+
+//DELETE USER
+router.delete("/:id",verifyTokenAndAdmin,async (req,res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted")
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
+//GET USER
+
+router.get("/find/:id",verifyTokenAndAdmin,async (req,res)=>{
+    try{
+        await User.findById(req.params.id)
+        res.status(200).json({
+            "User": User
+        })
+    }
+    catch(err){
+        res.status(500).json(err)
+    }
+})
+
 
 module.exports = router
